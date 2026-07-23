a551408bd7a15392484eeef84415ee4c69a3325b
Mehedi Jaman <mail4mjaman@gmail.com>
2026-07-23 20:08:36 +0600

    Add public developer meta information

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="developer" content="Mehedi Jaman">
+        <meta name="company" content="gTechservice">
+        <meta name="website" content="www.gtechservice.net">
+        <meta name="email" content="mail4gtechservice@gmail.com">
+        <meta name="phone" content="01841090747">
 
         {{-- Inline script to detect system dark mode preference and apply it immediately --}}
         <script>
